101bb9e87caf704736a316235831f1017ecf5aee
Josh Meredith <joshmeredith2008@gmail.com>
2017-05-13 00:52:01 +1000

    Rename now top level function goR to stencilAccess'

--- accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen/Stencil.hs
+++ accelerate-llvm/Data/Array/Accelerate/LLVM/CodeGen/Stencil.hs
@@ -48,14 +48,14 @@ stencilAccess
     -> CodeGen (IR stencil)
 stencilAccess mbndy arr =
   case mbndy of
-    Nothing   -> goR stencil (inbounds arr)
-    Just bndy -> goR stencil (bounded bndy arr)
+    Nothing   -> stencilAccess' stencil (inbounds arr)
+    Just bndy -> stencilAccess' stencil (bounded bndy arr)
 
 -- Base cases, nothing interesting to do here since we know the lower
 -- dimension is Z.
 --
-goR :: StencilR sh e stencil -> (IR sh -> CodeGen (IR e)) -> IR sh -> CodeGen (IR stencil)
-goR StencilRunit3 rf ix
+stencilAccess' :: StencilR sh e stencil -> (IR sh -> CodeGen (IR e)) -> IR sh -> CodeGen (IR stencil)
+stencilAccess' StencilRunit3 rf ix
   = let z :. i = unindex ix
         rf' d  = do d' <- A.add numType i (int d)
                     rf (index z d')
@@ -64,7 +64,7 @@ goR StencilRunit3 rf ix
          <*> rf   ix
          <*> rf'   1
 
-goR StencilRunit5 rf ix
+stencilAccess' StencilRunit5 rf ix
   = let z :. i = unindex ix
         rf' d  = do d' <- A.add numType i (int d)
                     rf (index z d')
@@ -75,7 +75,7 @@ goR StencilRunit5 rf ix
          <*> rf'   1
          <*> rf'   2
 
-goR StencilRunit7 rf ix
+stencilAccess' StencilRunit7 rf ix
   = let z :. i = unindex ix
         rf' d  = do d' <- A.add numType i (int d)
                     rf (index z d')
@@ -88,7 +88,7 @@ goR StencilRunit7 rf ix
          <*> rf'   2
          <*> rf'   3
 
-goR StencilRunit9 rf ix
+stencilAccess' StencilRunit9 rf ix
   = let z :. i = unindex ix
         rf' d  = do d' <- A.add numType i (int d)
                     rf (index z d')
@@ -108,57 +108,57 @@ goR StencilRunit9 rf ix
 -- when we recurse on the stencil structure we must manipulate the
 -- _innermost_ index component
 --
-goR (StencilRtup3 s1 s2 s3) rf ix =
+stencilAccess' (StencilRtup3 s1 s2 s3) rf ix =
   let (i, ix') = uncons ix
       rf' 0 ds = rf (cons i ds)
       rf' d ds = do d' <- A.add numType i (int d)
                     rf (cons d' ds)
   in
-  tup3 <$> goR s1 (rf' (-1)) ix'
-       <*> goR s2 (rf'   0)  ix'
-       <*> goR s3 (rf'   1)  ix'
+  tup3 <$> stencilAccess' s1 (rf' (-1)) ix'
+       <*> stencilAccess' s2 (rf'   0)  ix'
+       <*> stencilAccess' s3 (rf'   1)  ix'
 
-goR (StencilRtup5 s1 s2 s3 s4 s5) rf ix =
+stencilAccess' (StencilRtup5 s1 s2 s3 s4 s5) rf ix =
   let (i, ix') = uncons ix
       rf' 0 ds = rf (cons i ds)
       rf' d ds = do d' <- A.add numType i (int d)
                     rf (cons d' ds)
   in
-  tup5 <$> goR s1 (rf' (-2)) ix'
-       <*> goR s2 (rf' (-1)) ix'
-       <*> goR s3 (rf'   0)  ix'
-       <*> goR s4 (rf'   1)  ix'
-       <*> goR s5 (rf'   2)  ix'
+  tup5 <$> stencilAccess' s1 (rf' (-2)) ix'
+       <*> stencilAccess' s2 (rf' (-1)) ix'
+       <*> stencilAccess' s3 (rf'   0)  ix'
+       <*> stencilAccess' s4 (rf'   1)  ix'
+       <*> stencilAccess' s5 (rf'   2)  ix'
 
-goR (StencilRtup7 s1 s2 s3 s4 s5 s6 s7) rf ix =
+stencilAccess' (StencilRtup7 s1 s2 s3 s4 s5 s6 s7) rf ix =
   let (i, ix') = uncons ix
       rf' 0 ds = rf (cons i ds)
       rf' d ds = do d' <- A.add numType i (int d)
                     rf (cons d' ds)
   in
-  tup7 <$> goR s1 (rf' (-3)) ix'
-       <*> goR s2 (rf' (-2)) ix'
-       <*> goR s3 (rf' (-1)) ix'
-       <*> goR s4 (rf'   0)  ix'
-       <*> goR s5 (rf'   1)  ix'
-       <*> goR s6 (rf'   2)  ix'
-       <*> goR s7 (rf'   3)  ix'
+  tup7 <$> stencilAccess' s1 (rf' (-3)) ix'
+       <*> stencilAccess' s2 (rf' (-2)) ix'
+       <*> stencilAccess' s3 (rf' (-1)) ix'
+       <*> stencilAccess' s4 (rf'   0)  ix'
+       <*> stencilAccess' s5 (rf'   1)  ix'
+       <*> stencilAccess' s6 (rf'   2)  ix'
+       <*> stencilAccess' s7 (rf'   3)  ix'
 
-goR (StencilRtup9 s1 s2 s3 s4 s5 s6 s7 s8 s9) rf ix =
+stencilAccess' (StencilRtup9 s1 s2 s3 s4 s5 s6 s7 s8 s9) rf ix =
   let (i, ix') = uncons ix
       rf' 0 ds = rf (cons i ds)
       rf' d ds = do d' <- A.add numType i (int d)
                     rf (cons d' ds)
   in
-  tup9 <$> goR s1 (rf' (-4)) ix'
-       <*> goR s2 (rf' (-3)) ix'
-       <*> goR s3 (rf' (-2)) ix'
-       <*> goR s4 (rf' (-1)) ix'
-       <*> goR s5 (rf'   0)  ix'
-       <*> goR s6 (rf'   1)  ix'
-       <*> goR s7 (rf'   2)  ix'
-       <*> goR s8 (rf'   3)  ix'
-       <*> goR s9 (rf'   4)  ix'
+  tup9 <$> stencilAccess' s1 (rf' (-4)) ix'
+       <*> stencilAccess' s2 (rf' (-3)) ix'
+       <*> stencilAccess' s3 (rf' (-2)) ix'
+       <*> stencilAccess' s4 (rf' (-1)) ix'
+       <*> stencilAccess' s5 (rf'   0)  ix'
+       <*> stencilAccess' s6 (rf'   1)  ix'
+       <*> stencilAccess' s7 (rf'   2)  ix'
+       <*> stencilAccess' s8 (rf'   3)  ix'
+       <*> stencilAccess' s9 (rf'   4)  ix'
 
 
 stencilAccesses
@@ -314,18 +314,18 @@ stencilAccesses mbndy arr =
           rf' d ds = do d' <- A.add numType i (int d)
                         rf (cons d' ds)
       in do
-      x0 <- tup3 <$> goR s1 (rf' (-1)) ix'
-                 <*> goR s2 (rf'   0)  ix'
-                 <*> goR s3 (rf'   1)  ix'
-      x1 <- tup3 <$> goR s1 (rf'   0)  ix'
-                 <*> goR s2 (rf'   1)  ix'
-                 <*> goR s3 (rf'   2)  ix'
-      x2 <- tup3 <$> goR s1 (rf'   1)  ix'
-                 <*> goR s2 (rf'   2)  ix'
-                 <*> goR s3 (rf'   3)  ix'
-      x3 <- tup3 <$> goR s1 (rf'   2)  ix'
-                 <*> goR s2 (rf'   3)  ix'
-                 <*> goR s3 (rf'   4)  ix'
+      x0 <- tup3 <$> stencilAccess' s1 (rf' (-1)) ix'
+                 <*> stencilAccess' s2 (rf'   0)  ix'
+                 <*> stencilAccess' s3 (rf'   1)  ix'
+      x1 <- tup3 <$> stencilAccess' s1 (rf'   0)  ix'
+                 <*> stencilAccess' s2 (rf'   1)  ix'
+                 <*> stencilAccess' s3 (rf'   2)  ix'
+      x2 <- tup3 <$> stencilAccess' s1 (rf'   1)  ix'
+                 <*> stencilAccess' s2 (rf'   2)  ix'
+                 <*> stencilAccess' s3 (rf'   3)  ix'
+      x3 <- tup3 <$> stencilAccess' s1 (rf'   2)  ix'
+                 <*> stencilAccess' s2 (rf'   3)  ix'
+                 <*> stencilAccess' s3 (rf'   4)  ix'
       return (x0, x1, x2, x3)
 
     goR' (StencilRtup5 s1 s2 s3 s4 s5) rf ix =
@@ -334,26 +334,26 @@ stencilAccesses mbndy arr =
           rf' d ds = do d' <- A.add numType i (int d)
                         rf (cons d' ds)
       in do
-      x0 <- tup5 <$> goR s1 (rf' (-2)) ix'
-                 <*> goR s2 (rf' (-1)) ix'
-                 <*> goR s3 (rf'   0)  ix'
-                 <*> goR s4 (rf'   1)  ix'
-                 <*> goR s5 (rf'   2)  ix'
-      x1 <- tup5 <$> goR s1 (rf' (-1)) ix'
-                 <*> goR s2 (rf'   0)  ix'
-                 <*> goR s3 (rf'   1)  ix'
-                 <*> goR s4 (rf'   2)  ix'
-                 <*> goR s5 (rf'   3)  ix'
-      x2 <- tup5 <$> goR s1 (rf'   0)  ix'
-                 <*> goR s2 (rf'   1)  ix'
-                 <*> goR s3 (rf'   2)  ix'
-                 <*> goR s4 (rf'   3)  ix'
-                 <*> goR s5 (rf'   4)  ix'
-      x3 <- tup5 <$> goR s1 (rf'   1)  ix'
-                 <*> goR s2 (rf'   2)  ix'
-                 <*> goR s3 (rf'   3)  ix'
-                 <*> goR s4 (rf'   4)  ix'
-                 <*> goR s5 (rf'   5)  ix'
+      x0 <- tup5 <$> stencilAccess' s1 (rf' (-2)) ix'
+                 <*> stencilAccess' s2 (rf' (-1)) ix'
+                 <*> stencilAccess' s3 (rf'   0)  ix'
+                 <*> stencilAccess' s4 (rf'   1)  ix'
+                 <*> stencilAccess' s5 (rf'   2)  ix'
+      x1 <- tup5 <$> stencilAccess' s1 (rf' (-1)) ix'
+                 <*> stencilAccess' s2 (rf'   0)  ix'
+                 <*> stencilAccess' s3 (rf'   1)  ix'
+                 <*> stencilAccess' s4 (rf'   2)  ix'
+                 <*> stencilAccess' s5 (rf'   3)  ix'
+      x2 <- tup5 <$> stencilAccess' s1 (rf'   0)  ix'
+                 <*> stencilAccess' s2 (rf'   1)  ix'
+                 <*> stencilAccess' s3 (rf'   2)  ix'
+                 <*> stencilAccess' s4 (rf'   3)  ix'
+                 <*> stencilAccess' s5 (rf'   4)  ix'
+      x3 <- tup5 <$> stencilAccess' s1 (rf'   1)  ix'
+                 <*> stencilAccess' s2 (rf'   2)  ix'
+                 <*> stencilAccess' s3 (rf'   3)  ix'
+                 <*> stencilAccess' s4 (rf'   4)  ix'
+                 <*> stencilAccess' s5 (rf'   5)  ix'
       return (x0, x1, x2, x3)
 
     goR' (StencilRtup7 s1 s2 s3 s4 s5 s6 s7) rf ix =
@@ -362,34 +362,34 @@ stencilAccesses mbndy arr =
           rf' d ds = do d' <- A.add numType i (int d)
                         rf (cons d' ds)
       in do
-      x0 <- tup7 <$> goR s1 (rf' (-3)) ix'
-                 <*> goR s2 (rf' (-2)) ix'
-                 <*> goR s3 (rf' (-1)) ix'
-                 <*> goR s4 (rf'   0)  ix'
-                 <*> goR s5 (rf'   1)  ix'
-                 <*> goR s6 (rf'   2)  ix'
-                 <*> goR s7 (rf'   3)  ix'
-      x1 <- tup7 <$> goR s1 (rf' (-2)) ix'
-                 <*> goR s2 (rf' (-1)) ix'
-                 <*> goR s3 (rf'   0)  ix'
-                 <*> goR s4 (rf'   1)  ix'
-                 <*> goR s5 (rf'   2)  ix'
-                 <*> goR s6 (rf'   3)  ix'
-                 <*> goR s7 (rf'   4)  ix'
-      x2 <- tup7 <$> goR s1 (rf' (-1)) ix'
-                 <*> goR s2 (rf'   0)  ix'
-                 <*> goR s3 (rf'   1)  ix'
-                 <*> goR s4 (rf'   2)  ix'
-                 <*> goR s5 (rf'   3)  ix'
-                 <*> goR s6 (rf'   4)  ix'
-                 <*> goR s7 (rf'   5)  ix'
-      x3 <- tup7 <$> goR s1 (rf'   0)  ix'
-                 <*> goR s2 (rf'   1)  ix'
-                 <*> goR s3 (rf'   2)  ix'
-                 <*> goR s4 (rf'   3)  ix'
-                 <*> goR s5 (rf'   4)  ix'
-                 <*> goR s6 (rf'   5)  ix'
-                 <*> goR s7 (rf'   6)  ix'
+      x0 <- tup7 <$> stencilAccess' s1 (rf' (-3)) ix'
+                 <*> stencilAccess' s2 (rf' (-2)) ix'
+                 <*> stencilAccess' s3 (rf' (-1)) ix'
+                 <*> stencilAccess' s4 (rf'   0)  ix'
+                 <*> stencilAccess' s5 (rf'   1)  ix'
+                 <*> stencilAccess' s6 (rf'   2)  ix'
+                 <*> stencilAccess' s7 (rf'   3)  ix'
+      x1 <- tup7 <$> stencilAccess' s1 (rf' (-2)) ix'
+                 <*> stencilAccess' s2 (rf' (-1)) ix'
+                 <*> stencilAccess' s3 (rf'   0)  ix'
+                 <*> stencilAccess' s4 (rf'   1)  ix'
+                 <*> stencilAccess' s5 (rf'   2)  ix'
+                 <*> stencilAccess' s6 (rf'   3)  ix'
+                 <*> stencilAccess' s7 (rf'   4)  ix'
+      x2 <- tup7 <$> stencilAccess' s1 (rf' (-1)) ix'
+                 <*> stencilAccess' s2 (rf'   0)  ix'
+                 <*> stencilAccess' s3 (rf'   1)  ix'
+                 <*> stencilAccess' s4 (rf'   2)  ix'
+                 <*> stencilAccess' s5 (rf'   3)  ix'
+                 <*> stencilAccess' s6 (rf'   4)  ix'
+                 <*> stencilAccess' s7 (rf'   5)  ix'
+      x3 <- tup7 <$> stencilAccess' s1 (rf'   0)  ix'
+                 <*> stencilAccess' s2 (rf'   1)  ix'
+                 <*> stencilAccess' s3 (rf'   2)  ix'
+                 <*> stencilAccess' s4 (rf'   3)  ix'
+                 <*> stencilAccess' s5 (rf'   4)  ix'
+                 <*> stencilAccess' s6 (rf'   5)  ix'
+                 <*> stencilAccess' s7 (rf'   6)  ix'
       return (x0, x1, x2, x3)
 
     goR' (StencilRtup9 s1 s2 s3 s4 s5 s6 s7 s8 s9) rf ix =
@@ -398,42 +398,42 @@ stencilAccesses mbndy arr =
           rf' d ds = do d' <- A.add numType i (int d)
                         rf (cons d' ds)
       in do
-      x0 <- tup9 <$> goR s1 (rf' (-4)) ix'
-                 <*> goR s2 (rf' (-3)) ix'
-                 <*> goR s3 (rf' (-2)) ix'
-                 <*> goR s4 (rf' (-1)) ix'
-                 <*> goR s5 (rf'   0)  ix'
-                 <*> goR s6 (rf'   1)  ix'
-                 <*> goR s7 (rf'   2)  ix'
-                 <*> goR s8 (rf'   3)  ix'
-                 <*> goR s9 (rf'   4)  ix'
-      x1 <- tup9 <$> goR s1 (rf' (-3)) ix'
-                 <*> goR s2 (rf' (-2)) ix'
-                 <*> goR s3 (rf' (-1)) ix'
-                 <*> goR s4 (rf'   0)  ix'
-                 <*> goR s5 (rf'   1)  ix'
-                 <*> goR s6 (rf'   2)  ix'
-                 <*> goR s7 (rf'   3)  ix'
-                 <*> goR s8 (rf'   4)  ix'
-                 <*> goR s9 (rf'   5)  ix'
-      x2 <- tup9 <$> goR s1 (rf' (-2)) ix'
-                 <*> goR s2 (rf' (-1)) ix'
-                 <*> goR s3 (rf'   0)  ix'
-                 <*> goR s4 (rf'   1)  ix'
-                 <*> goR s5 (rf'   2)  ix'
-                 <*> goR s6 (rf'   3)  ix'
-                 <*> goR s7 (rf'   4)  ix'
-                 <*> goR s8 (rf'   5)  ix'
-                 <*> goR s9 (rf'   6)  ix'
-      x3 <- tup9 <$> goR s1 (rf' (-1)) ix'
-                 <*> goR s2 (rf'   0)  ix'
-                 <*> goR s3 (rf'   1)  ix'
-                 <*> goR s4 (rf'   2)  ix'
-                 <*> goR s5 (rf'   3)  ix'
-                 <*> goR s6 (rf'   4)  ix'
-                 <*> goR s7 (rf'   5)  ix'
-                 <*> goR s8 (rf'   6)  ix'
-                 <*> goR s9 (rf'   7)  ix'
+      x0 <- tup9 <$> stencilAccess' s1 (rf' (-4)) ix'
+                 <*> stencilAccess' s2 (rf' (-3)) ix'
+                 <*> stencilAccess' s3 (rf' (-2)) ix'
+                 <*> stencilAccess' s4 (rf' (-1)) ix'
+                 <*> stencilAccess' s5 (rf'   0)  ix'
+                 <*> stencilAccess' s6 (rf'   1)  ix'
+                 <*> stencilAccess' s7 (rf'   2)  ix'
+                 <*> stencilAccess' s8 (rf'   3)  ix'
+                 <*> stencilAccess' s9 (rf'   4)  ix'
+      x1 <- tup9 <$> stencilAccess' s1 (rf' (-3)) ix'
+                 <*> stencilAccess' s2 (rf' (-2)) ix'
+                 <*> stencilAccess' s3 (rf' (-1)) ix'
+                 <*> stencilAccess' s4 (rf'   0)  ix'
+                 <*> stencilAccess' s5 (rf'   1)  ix'
+                 <*> stencilAccess' s6 (rf'   2)  ix'
+                 <*> stencilAccess' s7 (rf'   3)  ix'
+                 <*> stencilAccess' s8 (rf'   4)  ix'
+                 <*> stencilAccess' s9 (rf'   5)  ix'
+      x2 <- tup9 <$> stencilAccess' s1 (rf' (-2)) ix'
+                 <*> stencilAccess' s2 (rf' (-1)) ix'
+                 <*> stencilAccess' s3 (rf'   0)  ix'
+                 <*> stencilAccess' s4 (rf'   1)  ix'
+                 <*> stencilAccess' s5 (rf'   2)  ix'
+                 <*> stencilAccess' s6 (rf'   3)  ix'
+                 <*> stencilAccess' s7 (rf'   4)  ix'
+                 <*> stencilAccess' s8 (rf'   5)  ix'
+                 <*> stencilAccess' s9 (rf'   6)  ix'
+      x3 <- tup9 <$> stencilAccess' s1 (rf' (-1)) ix'
+                 <*> stencilAccess' s2 (rf'   0)  ix'
+                 <*> stencilAccess' s3 (rf'   1)  ix'
+                 <*> stencilAccess' s4 (rf'   2)  ix'
+                 <*> stencilAccess' s5 (rf'   3)  ix'
+                 <*> stencilAccess' s6 (rf'   4)  ix'
+                 <*> stencilAccess' s7 (rf'   5)  ix'
+                 <*> stencilAccess' s8 (rf'   6)  ix'
+                 <*> stencilAccess' s9 (rf'   7)  ix'
       return (x0, x1, x2, x3)
 
 
